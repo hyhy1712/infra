f740d3014f05ec980d327d0bcb044ef2631882d9
Tuấn Hiệp <jhdongchul@gmail.com>
2025-08-06 15:49:14 +0700

    init kafka, filebeat

--- php-fpm/study/start.sh
+++ php-fpm/study/start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+mkdir -p /var/www/study/storage/logs /var/www/study/bootstrap/cache
+
+chown -R www-data:www-data /var/www/study/storage /var/www/study/bootstrap/cache
+chmod -R 775 /var/www/study/storage /var/www/study/bootstrap/cache
+
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf &
 
 php-fpm
